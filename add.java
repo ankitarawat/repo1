d6afcaa44425226982df282e22ca7b7c9aefc0e6
ankita <aaankita.rawat@gmail.com>
2016-11-11 10:02:50 +0000

    1st commit

---
+++ add.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;
+ 
+public class add
+{
+   public static void main(String args[])
+   {
+      int x=3, y=4, z;
+      System.out.println("Enter two integers to calculate their sum ");
+     // Scanner in = new Scanner(System.in);
+     // x = in.nextInt();
+     // y = in.nextInt();
+      z = x + y;
+      System.out.println("Sum of entered integers = "+z);
+   }
+}
